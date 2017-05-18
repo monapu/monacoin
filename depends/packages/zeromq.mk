fc325005b0a29d5fac6e5c56c5747ff4c40ac9e6
monapu <mona@mona>
2017-05-18 09:29:01 +0900

    Enable zeromq on Windows builds

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -1,11 +1,11 @@
 package=zeromq
-$(package)_version=4.1.4
-$(package)_download_path=http://download.zeromq.org
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=e99f44fde25c2e4cb84ce440f87ca7d3fe3271c2b8cfbc67d55e4de25e6fe378
+$(package)_version=4.2.0
+$(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v4.2.0/
+$(package)_file_name=zeromq-4.2.0.tar.gz
+$(package)_sha256_hash=53b83bf0ee978931f76fa9cb46ad4affea65787264a5f3d140bc743412d0c117
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-documentation --disable-shared --without-libsodium
+  $(package)_config_opts=--without-documentation --disable-shared --without-libsodium --disable-curve
   $(package)_config_opts_linux=--with-pic
   $(package)_cxxflags=-std=c++11
 endef
@@ -15,7 +15,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) libzmq.la
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
